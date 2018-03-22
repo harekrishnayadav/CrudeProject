6ff08fef2be0590647e2ed16b352a901278affd3
sujeet <harekrishna.bhumca13@gmai98l.com>
2018-03-22 15:00:51 +0530

    projectCreation

---
+++ pringBootRestfulWithEmbedded/src/main/java/com/example/demo/PringBootRestfulWithEmbeddedApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PringBootRestfulWithEmbeddedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PringBootRestfulWithEmbeddedApplication.class, args);
+	}
+}
