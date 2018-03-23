f0973ce9323e35ba81dc4379645696cf25fdff60
sujeet <harekrishna.bhumca13@gmai98l.com>
2018-03-23 12:46:08 +0530

    BankProject

---
+++ IndianBank/src/main/java/indian/bank/IndianBankApplication.java
@@ -0,0 +1,12 @@
+package indian.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndianBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndianBankApplication.class, args);
+	}
+}
---
+++ IndianBank/src/test/java/indian/bank/IndianBankApplicationTests.java
@@ -0,0 +1,16 @@
+package indian.bank;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IndianBankApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
